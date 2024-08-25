dcc5eafc51d87180847393753e96ed82f582678f
Philip <f_suprun@mail.ru>
2024-08-25 20:39:35 +0300

    refactoring 0.8 add thanks page, add new pictures

--- send.php
+++ send.php
@@ -4,12 +4,12 @@ $name=&_POST['name'];
 $phone=$__POST['phone'];
 $what=&_POST['what'];
 // Тут указываем на какой ящик посылать письмо
-$to = "f_suprun@mail.ru";
+$to = "Zabory_lyx@mail.ru";
 // Далее идет тема и само сообщение
 $subject = "Заявка с сайта SGG";
 $message = "
 Письмо отправлено из моей формы.<br />
-Пользователь хочетЖ ".htmlspecialchars($what)."<br />
+Пользователь хочет: ".htmlspecialchars($what)."<br />
 Имя: ".htmlspecialchars($name)."<br />
 Телефон: ".htmlspecialchars($phone)."<br />;
 $headers = "From: mysite.ru <site-email@mysite.ru>\r\nContent-type: text/html";
