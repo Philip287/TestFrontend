ac9dea16f57fc9fbabbf900cf5690778299c9576
Philip <f_suprun@mail.ru>
2024-08-29 17:29:03 +0300

    refactoring 1.2 fix send php, add validation

--- send.php
+++ send.php
@@ -1,20 +1,21 @@
 <?php
 //Принимаем почтовые данные
-$name=&_POST['name'];
-$phone=$__POST['phone'];
-$what=&_POST['what'];
+$name=$_POST['name'];
+$phone=$_POST['phone'];
+$what=$_POST['what'];
+$question=$_POST['question'];
 // Тут указываем на какой ящик посылать письмо
 $to = "Zabory_lyx@mail.ru";
 // Далее идет тема и само сообщение
-$subject = "Заявка с сайта SGG";
+$subject = "Заявка с сайта smart gard";
 $message = "
-Письмо отправлено из моей формы.<br />
-Пользователь хочет: ".htmlspecialchars($what)."<br />
-Имя: ".htmlspecialchars($name)."<br />
-Телефон: ".htmlspecialchars($phone)."<br />;
-$headers = "From: mysite.ru <site-email@mysite.ru>\r\nContent-type: text/html";
-charset=utf8\r\n";
+Письмо отправлено из моей формы на сайте smart gard. <br>
+Пользователь хочет: ".htmlspecialchars($what). "<br>
+Имя: ".htmlspecialchars($name)."<br>
+Телефон: ".htmlspecialchars($phone)."<br>
+Вопрос: ".htmlspecialchars($question)."<br>";
+$headers = "From: crimeaimplant.ru <info@crimeaimplant.ru>\r\nContent-type: text/html; charset=UTF-8 \r\n";
 mail ($to, $subject, $message, $headers);
 header('location: thanks.html');
-exit();
+exit;
 ?>
